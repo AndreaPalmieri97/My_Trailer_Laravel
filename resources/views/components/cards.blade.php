0cf2bd6e207a890128f6c84b2ca4818297f36822
Andrea Palmieri <palmersbari@gmail.com>
2023-04-26 16:12:57 +0200

    Modificato stile bottoni

--- resources/views/components/cards.blade.php
+++ resources/views/components/cards.blade.php
@@ -1,9 +1,9 @@
 <div class="card border border-danger rounded" style="width: 15rem;">
     <img src="{{ Storage::url($trailer->img)}}" class="card-img-top" alt="...">
-    <div class="card-body bg-dark">
+    <div class="card-body bg-dark d-flex flex-column justify-content-center">
       <h5 class="card-title text-center fw-semibold text-danger">{!! $trailer->title !!}</h5>
-      <a href="{{route('trailers.show', $trailer)}}" class="btn btn-success my-3 d-flex flex-column align-items-center ">Guarda Trailer</a>
-      <a href="{{route('trailers.edit', $trailer)}}" class="btn btn-warning my-3 d-flex flex-column align-items-center ">Modifica Trailer</a>
-      <a href="{{route('trailers.delete', $trailer)}}" class="btn btn-danger my-3 d-flex flex-column align-items-center ">Elimina Trailer</a>
+      <a href="{{route('trailers.show', $trailer)}}" class="my-1 btn btn-success text-center text-white">Guarda <i class="fa-solid fa-video text-white"></i></a>
+      <a href="{{route('trailers.edit', $trailer)}}" class="my-1 btn btn-warning text-center text-white">Modifica <i class="fa-solid fa-pen-to-square text-white"></i></a>
+      <a href="{{route('trailers.delete', $trailer)}}" class="my-1 btn btn-danger text-center">Elimina <i class="fa-solid fa-trash-can text-white mt-1"></i></a>
     </div>
   </div>
--- resources/views/trailers/edit.blade.php
+++ resources/views/trailers/edit.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row my-5">
             <div class="col-12">
-                <h1 class="text-danger"><i class="fa-solid fa-pen-to-square text-white"></i></i> Modifica il Trailer</h1>
+                <h1 class="text-danger"><i class="fa-solid fa-pen-to-square text-white"></i> Modifica il Trailer</h1>
             </div>
         </div>
     </div>
